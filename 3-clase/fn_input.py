cb3da35868e98dd6a3081d2d44810343314c5b05
Martin Centurion <martinalejandrocenturion@gmail.com>
2024-09-25 21:40:20 -0300

    clase 3

---
+++ 3-clase/fn_input.py
@@ -0,0 +1,14 @@
+#Funcion Input
+
+#nombre = input("Introduce tu nombre: ");
+#print(f"Hola {nombre}")
+
+
+#Input por defecto el valor ingresado es un string
+numero_uno = int(input("Ingrese un número: "))
+numero_dos = input("Ingrese segundo numero: ")
+print(type(numero_uno))
+print(type(numero_dos))
+suma = numero_uno + int(numero_dos)
+
+print(f"La suma es {suma}")
---
+++ 3-clase/operadores.py
@@ -0,0 +1,34 @@
+#Operadores 
+
+#F-String f"PUEDO PONER CUALQUIER TEXTO {resta}"
+
+numero_uno = 20
+numero_dos = 5.545454
+
+resta = numero_uno - numero_dos
+
+print(f"La resta entre {numero_uno} y {numero_dos} es {resta}")
+
+multiplicacion = numero_uno * numero_dos
+print(f"La multiplicación es: {multiplicacion}")
+
+division = numero_uno / numero_dos
+print(f"La division es {division}")
+
+division_entera = numero_uno // numero_dos
+print(f"La division entera: {division_entera} / tipo {type(division_entera)}")
+
+#Resto de la division
+
+resto = numero_uno % numero_dos
+print(f"El resto de la division: {resto}")
+
+#Potencia
+
+potencia = 10 ** 3
+print(f"La potencia es {potencia}")
+
+#Raiz
+
+raiz_cuadrada = 4 ** 0.5
+print(f"La raiz cuadrada es: {raiz_cuadrada}")
