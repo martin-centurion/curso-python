04c5bcb5459f6e2951f72b6e8af6c3c35c6c23e1
Martin Centurion <martinalejandrocenturion@gmail.com>
2024-10-07 22:01:12 -0300

    crud

--- 4-clase/resolucion_ejercicios.py
+++ 4-clase/resolucion_ejercicios.py
@@ -12,8 +12,48 @@ resta = numero1 - numero2
 multiplicacion = numero1 * numero2
 modulo = numero1 % numero2
 
-print(f"La suma de los numeros es: {suma}");
-print(f"La resta de los numeros es: {resta}");
-print(f"La multiplicación de los numeros es: {multiplicacion}");
-print(f"El modulo de los numeros es: {modulo}");
+print(f"La suma de los numeros es: {suma}")
+print(f"La resta de los numeros es: {resta}")
+print(f"La multiplicación de los numeros es: {multiplicacion}")
+print(f"El modulo de los numeros es: {modulo}")
+
+"""
+Escribe un programa en Python que calcule la propina que se debe dejar en un restaurante.
+
+El script debe solicitar al usuario el monto total de la cuenta y el porcentaje de propina que desea dejar.
+
+Utilizando operadores aritméticos, calcula la cantida de propinas y el total a pagar (incluyendo la propina).
+Finalmente, muestra los resultados en la pantalla.
+"""
+
+monto_total = float(input("Ingrese el monto total: "))
+porcentaje_propina = float(input("Porcentaje de propina: "))
+
+propina = monto_total * (porcentaje_propina / 100)
+
+total_pagar = monto_total + propina
+
+print(f"La cantidad de propina es: ${propina}")
+print(f"El total a pagar incluida la propina es: ${total_pagar}")
+
+"""
+Rubros:
+
+Ecommerce donde se pueda aplicar el conocimiento de Python:
+
+Seleccionar rubro: Tienda de Ropa - JCR INDUMENTARIA
+
+Aplicar CRUD - Create / Read / Updated / Delete
+Donde se puede eliminar el producto, modificar, cargar y eliminar
+el diseño debe ser minimalista.
+
+Colores solidos: 
+Violeta #220B42 - #6D30C4
+Naranja #FF8400 - #FFA23E
+Blanco #FFFFFF
+
+Tratar de que estos colores sean pasteles - Buscar una paleta de
+colores acorde al ecommerce desarrollado.
+
+"""
 
