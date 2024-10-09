f976e9e65217e4e61cdf9e3a91f371663f43dbf6
Martin Centurion <martinalejandrocenturion@gmail.com>
2024-10-09 19:51:13 -0300

    inicio clase 5

---
+++ 5-clase/cadenas.py
@@ -0,0 +1,39 @@
+cadena1 = "Hola Cadena"
+cadena2 = 'Python es genial'
+cadena3 = '''Es una cadena que permite el
+            salto de linea'''
+
+# Operacion con cadenas
+# Repeteción
+parte1 = "Na " * 6 # Si se suma da error
+print(parte1)
+
+# Concatenación +
+cancion = parte1 + "Batman"
+print(cancion)
+
+# Longitud de la cadena
+longitud = len(cancion)
+print(longitud)
+
+# Slicing - rebanadas
+texto = "Python es divertido"
+print(texto[18]) # acceder a un caracter por su posición
+
+# Extraer una subcadena - [inicio - fin]
+print(texto[0:2])
+
+# Omitir el inicio
+print(texto[:8])
+
+# Omitir al final
+print(texto[7:])
+
+#agregar un paso/salto - Saltea las posiciones de una cadena de texto
+print(texto[0:10:2])
+
+# Indices negativos - Uso de indices negativos
+print(texto[-9:0]) # Comienza 9 posiciones antes del final
+
+#
+
---
+++ 5-clase/resolucion.py
@@ -0,0 +1,21 @@
+"""
+Realizar una aplicación en Python...
+"""
+
+# ENTRADA Lts_100km(float) - costo_lt (float) - longitud_viaje (float)
+litros_por_100km = float(input(""))
+
+costo_por_litro = float(input(""))
+
+longitud_viaje = float(input(""))
+
+# PROCESO - Calcular los litros consumidos y el precio final
+litros_consumidos = (longitud_viaje * litros_por_100km) / 100
+
+#costo total
+costo_total = litros_consumidos * costo_por_litro
+
+# SALIDA - Litros consumidos y el precio final
+print(f"...: {litros_consumidos} 1")
+print(f"... : $")
+
