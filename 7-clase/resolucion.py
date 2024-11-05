800d7aa0627e00a77f13caeafee9d4aed48a5169
Martin Centurion <martinalejandrocenturion@gmail.com>
2024-11-05 02:13:35 -0300

    inicio repaso clase 7W

---
+++ 7-clase/resolucion.py
@@ -0,0 +1,9 @@
+"""
+Desarrolla un programa que permita al usuario ingresar el nombre de varios productis
+y la cantidad de stock que hay de cada uno. El programa debe seguir pidiendo que ingrese 
+...
+"""
+
+contador_productos = 0
+acumulador_stock = 0
+
