3a0465434714847b3407ddc058250354b057fd00
Martin Centurion <martinalejandrocenturion@gmail.com>
2024-11-05 15:00:47 -0300

    [Clase 8] Inicio repaso

---
+++ 8-clase/resolucion.py
@@ -0,0 +1,7 @@
+"""
+Tu programa debe permitir al usuario consultar el inventario de
+una tienda para verificar si un producto está en stock.
+Si el producto está en la lista, el programa debe informarlo, si no,
+debe mostrar un mensaje indicando que no está disponible
+"""
+
